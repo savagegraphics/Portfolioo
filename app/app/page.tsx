b56ea60431716d5ea8d9381a7d31018ca274d19c
olawaleraji <savagedotjsx@gmail.com>
2024-03-25 09:23:45 -0700

    check now

---
+++ app/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+// import React from "react";
+import Navbar from "./Navbar";
+import About from "./About";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="">
+      <Navbar />
+      {/* <About/> */}
+    </div>
+  );
+};
+
+export default page;
