91c5270a067a1a681f5d54ad71f93842053599d0
Project SURVIVOR <161699187+olawaleraji@users.noreply.github.com>
2024-03-28 13:58:16 +0100

    wetin dey paa

--- app/Home.tsx/Hero.tsx
+++ app/Home.tsx/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = (props: Props) => {
       <div className="max-w-screen-2xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
         <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
           <h1 className="text-sm text-indigo-600 font-medium">
-            Over 200 successful deals
+            Over 20 successful deals
           </h1>
           <h2 className="text-3xl text-gray-800 font-extrabold md:text-5xl">
             Hi👋🏽, <br />
