b6c190335143a059db179b08e5ad74db8f38b537
olawaleraji <savagedotjsx@gmail.com>
2024-04-17 08:39:54 -0700

    My commit

--- app/Home.tsx/Card2.tsx
+++ app/Home.tsx/Card2.tsx
@@ -88,34 +88,6 @@ const Card = (props: Props) => {
             </div>
           </div>
         </Link>
-        <Link href='https://portfolioo-vert.vercel.app/'>
-          <div className='overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm'>
-            <img
-              src='./bwoi/MyPortfolio.png'
-              className='object-contain w-full'
-              alt=''
-            />
-            <div className='p-5 border border-t-0'>
-              <p className='mb-3 text-xs font-semibold tracking-wide uppercase'>
-                <a
-                  href='/'
-                  className='transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700'
-                  aria-label='Category'
-                  title='traveling'
-                >
-                  My Portfolio
-                </a>
-              </p>
-              <p className='mb-2 text-gray-700'>
-                My Portfolio is a showcase of various web applications,
-                including RouteRover, Babylist, Happie, Boss Maintenance, and
-                Getlinked. Utilizing technologies like NEXT.js, React, NextUi,
-                and Tailwind CSS, it provides a comprehensive display of
-                projects and skills.
-              </p>
-            </div>
-          </div>
-        </Link>
         <Link href='https://traveler-beta.vercel.app/'>
           <div className='overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm'>
             <img
