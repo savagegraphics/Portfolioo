13eaa9821d1884edec44ee208705eb9208c9a2f2
olawaleraji <savagedotjsx@gmail.com>
2024-04-17 07:13:27 -0700

    My commit

--- app/Home.tsx/Hero.tsx
+++ app/Home.tsx/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Bwoi from '@/public/bwoiyo.jpg'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -28,30 +29,12 @@ const Hero = (props: Props) => {
             collaborate to turn your ideas into captivating user experiences!
           </p>
           <div className='items-center gap-x-3 space-y-3 sm:flex sm:space-y-0'>
-            <a
-              href='javascript:void(0)'
+            <Link
+              href='/Home.tsx/Contact'
               className='block py-2 px-4 text-center text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none'
             >
-              Let&apos;s get started
-            </a>
-            <a
-              href='javascript:void(0)'
-              className='flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex'
-            >
-              Get access
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='w-5 h-5'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </a>
+              Get In Touch
+            </Link>
           </div>
         </div>
         <div className='flex-none mt-14 md:mt-0 md:max-w-xl'>
